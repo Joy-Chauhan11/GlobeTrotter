508702c3e502077352335cc005997dd313a1b975
Joy Chauhan <joychauhan1104@gmail.com>
2026-08-22 11:24:12 +0530

    Chages in login and register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
+import { SignIn } from '@clerk/clerk-react';
+import { SignUp } from '@clerk/clerk-react';
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/clerk/sign-in" element={<SignIn routing="path" path="/clerk/sign-in" />} />
+        <Route path="/clerk/sign-up" element={<SignUp routing="path" path="/clerk/sign-up" />} />
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
